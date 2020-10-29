df61c83051f0a74093b77b4084d2540ffd27672f
Lilian Montero <lilianmonterolopez@gmail.com>
2020-10-29 17:59:51 -0300

    Improvements styles

--- src/view/App.js
+++ src/view/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import Jumbotron from './components/Jumbotron';
+import Navbar from '../components/Navbar';
+import Jumbotron from '../components/Jumbotron';
 import MyCards from '../components/MyCards';
 import Footer from '../components/Footer';
 
 function App() {
   return (
     <>
-    <Navbar />
-    <div className="container ">
-      <Jumbotron/>
-      <div className="row mt-1 mb-2">
-        <MyCards />
+      <Navbar />
+      <div className="container">
+        <div className="row p-3">
+          <Jumbotron />
+        </div>
+        <div className="row mt-1 mb-2 p-1">
+          <MyCards />
+        </div>
       </div>
       <Footer copyright="Copyright © React Website" />
-    </div>
     </>
   );
 }
